346731b0e46e2e6db10df7ca6195a181cbb316a8
Shawn Miller <hereiamnow@gmail.com>
2020-01-30 14:41:46 -0600

    added index.js exports

---
+++ src/components/Disqus/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by S. Miller <hereiamnow@gmail.com>
+ * Date: 1/30/2020 2:27 PM
+ * Relative Path: src/components/Disqus/index.js
+ * Description: Directory Index for default class for imports
+ */
+
+export { default } from './Disqus';
---
+++ src/components/Team/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by S. Miller <hereiamnow@gmail.com>
+ * Date: 1/30/2020 2:30 PM
+ * Relative Path: src/components/Team/index.js
+ * Description: Directory Index for default class for imports
+ */
+
+export { default } from './Team';
